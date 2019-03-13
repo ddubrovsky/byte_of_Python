2c9be8866ae92d33caaeb00253cfc140005fec63
Dmitrii <kddima1976@gmail.com>
2019-03-13 21:39:47 +0400

    The return statement and DocStrings

---
+++ function_return.py
@@ -0,0 +1,13 @@
+def maximum(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return 'The numbers are equal'
+    else:
+        return y
+
+print(maximum(2, 3))
+
+
+def some_function():
+    pass # the pass statement is used in Python to indicate an empty block of statements
